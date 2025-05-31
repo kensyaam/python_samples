b947b15dcbf1fa32ba6d18453fa61603d18ced44
kensyaam <kensyaam@example.com>
2025-05-31 21:12:18 +0900

    format

--- Azure_Functions/api/blueprint.py
+++ Azure_Functions/api/blueprint.py
@@ -5,16 +5,14 @@
 # Please refer to https://aka.ms/azure-functions-python-blueprints
 
 
-import logging
-import azure.functions as func
-
-import os
 import json
+import logging
+import os
 from datetime import datetime
 
-from services import psycopg_sample
-from services import sqlalchemy_sample
+import azure.functions as func
 
+from services import psycopg_sample, sqlalchemy_sample
 
 blueprint = func.Blueprint()
 
@@ -34,7 +32,10 @@ def get_current_datetime() -> datetime:
             return datetime.strptime(for_test, "%Y-%m-%d %H:%M:%S")
         except ValueError as e:
             # raise ValueError("環境変数 TEST_DATETIME の値が日付フォーマットと一致しません。") from e
-            logging.debug("環境変数 TEST_DATETIME の値が日付フォーマットと一致しません。 (%s)", str(e))
+            logging.debug(
+                "環境変数 TEST_DATETIME の値が日付フォーマットと一致しません。 (%s)",
+                str(e),
+            )
 
     # 環境変数が設定されていない場合は、現在の日時を取得
     return datetime.now()
@@ -42,110 +43,100 @@ def get_current_datetime() -> datetime:
 
 @blueprint.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    name = req.params.get('name')
+    logging.debug("DEBUG level log")
+    logging.info("INFO level log")
+    logging.warning("WARNING level log")
+    logging.error("ERROR level log")
+    logging.critical("CRITICAL level log")
+
+    name = req.params.get("name")
     if not name:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get("name")
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(
+            f"Hello, {name}. This HTTP triggered function executed successfully."
+        )
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200,
         )
 
 
 @blueprint.route(route="db_access_sample", auth_level=func.AuthLevel.ANONYMOUS)
 def db_access_whithout_sqlalchemy(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
     try:
         products = psycopg_sample.select()
 
         return func.HttpResponse(
-            body=json.dumps(products),
-            mimetype="application/json",
-            status_code=200
+            body=json.dumps(products), mimetype="application/json", status_code=200
         )
 
     except Exception as e:
-        return func.HttpResponse(
-            body=f"Error: {str(e)}",
-            status_code=500
-        )
+        return func.HttpResponse(body=f"Error: {str(e)}", status_code=500)
 
 
 @blueprint.route(route="db_select_sample", auth_level=func.AuthLevel.ANONYMOUS)
 def db_select_sample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
     try:
         products = sqlalchemy_sample.select()
 
         return func.HttpResponse(
-            body=json.dumps(products),
-            mimetype="application/json",
-            status_code=200
+            body=json.dumps(products), mimetype="application/json", status_code=200
         )
 
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
-        return func.HttpResponse(
-            body=f"Error: {str(e)}",
-            status_code=500
-        )
+        return func.HttpResponse(body=f"Error: {str(e)}", status_code=500)
+
 
 @blueprint.route(route="db_upsert_sample", auth_level=func.AuthLevel.ANONYMOUS)
 def db_upsert_sample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    product_id = req.params.get('product_id')
+    product_id = req.params.get("product_id")
 
     order_data = {
         "order_id": 6,  # 主キーを指定
         "customer_id": 4,
         "product_id": product_id,
         "quantity": 3,
-        "order_date": get_current_datetime()
+        "order_date": get_current_datetime(),
     }
 
     try:
         sqlalchemy_sample.upsert(order_data)
 
-        return func.HttpResponse(
-            status_code=200
-        )
+        return func.HttpResponse(status_code=200)
 
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
-        return func.HttpResponse(
-            body=f"Error: {str(e)}",
-            status_code=500
-        )
+        return func.HttpResponse(body=f"Error: {str(e)}", status_code=500)
+
 
 @blueprint.route(route="db_delete_sample", auth_level=func.AuthLevel.ANONYMOUS)
 def db_delete_sample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
-    product_id = req.params.get('product_id')
+    product_id = req.params.get("product_id")
 
     try:
         sqlalchemy_sample.delete(int(product_id))
 
-        return func.HttpResponse(
-            status_code=200
-        )
+        return func.HttpResponse(status_code=200)
 
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
-        return func.HttpResponse(
-            body=f"Error: {str(e)}",
-            status_code=500
-        )
+        return func.HttpResponse(body=f"Error: {str(e)}", status_code=500)
